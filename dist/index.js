b3b01ffee31e8735f55c7a1b36a644fce8ee2da6
Brett Hopper <brett.hoppz@gmail.com>
2024-01-30 13:55:37 +1100

    Tweaked card text: * Removed potential actions * Included full commit message in the body of the card

--- dist/index.js
+++ dist/index.js
@@ -2456,23 +2456,8 @@ function createMessageCard(notificationSummary, notificationColor, commit, autho
         title: notificationSummary,
         sections: [
             {
-                activityTitle: `**CI #${runNum} (commit ${sha.substr(0, 7)})** on [${repoName}](${repoUrl})`,
-                activityImage: avatar_url,
-                activitySubtitle: `by ${commit.data.commit.author.name} [(@${author.login})](${author.html_url}) on ${timestamp}`
-            }
-        ],
-        potentialAction: [
-            {
-                '@context': 'http://schema.org',
-                target: [`${repoUrl}/actions/runs/${runId}`],
-                '@type': 'ViewAction',
-                name: 'View Workflow Run'
-            },
-            {
-                '@context': 'http://schema.org',
-                target: [commit.data.html_url],
-                '@type': 'ViewAction',
-                name: 'View Commit Changes'
+                activitySubtitle: `${commit.data.commit.message}`,
+                activityImage: avatar_url
             }
         ]
     };
